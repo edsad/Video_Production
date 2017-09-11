3f17fd245e6db2e502e8ebae4cc660c79d07932f
edsad <ed_lorie@comcast.net>
2017-09-11 10:04:53 -0500

    added Models

---
+++ Video_Production/Models/Production.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video_Production.Models
+{
+    public class Production
+    {
+    }
+}
